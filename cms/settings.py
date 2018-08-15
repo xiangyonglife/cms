d233631cc61e54d204963464ee03b64ea0613e4b
xiangyong <cc.123>
2018-08-15 18:17:43 +0800

    添加忽略文件

--- cms/settings.py
+++ cms/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'cms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +74,15 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# 数据的配置
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cms',
+        'HOST': '118.25.177.218',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'PORT': '3306',
     }
 }
 
